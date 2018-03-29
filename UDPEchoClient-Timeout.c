cb114938b83df6482e4d3ca79982524b852aca7a
wulikanhua <1113513062@qq.com>
2018-03-29 15:17:13 +0800

    UDP客户端超时处理

---
+++ UDPEchoClient-Timeout.c
@@ -0,0 +1,98 @@
+//
+// Created by tw on 2018/3/7.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <w32api/ws2tcpip.h>
+#include "Practical.h"
+
+static const unsigned int TIMEOUT_SECS = 2;
+static const unsigned int MAXTRIES = 5;
+
+unsigned int tries = 0;
+
+void CatchAlarm(int ignored);
+
+int main(int argc, char *argv[]) {
+
+    if ((argc < 3 ) || (argc > 4))
+        DieWithUserMessage("Parameter(s)", "<Server Address/Name> <Echo Word> [<Server Port/Service>]\n");
+
+    char *server = argv[1];
+    char *echoString = argv[2];
+
+    size_t echoStringLen = strlen(echoString);
+    if (echoString > 1024)
+        DieWithUserMessage(echoString, "too long");
+
+    char *service = (argc == 4) ? argv[3] : "echo";
+
+    struct addrinfo addrCriteris;
+    memset(&addrCriteris, 0, sizeof(addrCriteris));
+    addrCriteris.ai_family = AF_UNSPEC;
+    addrCriteris.ai_socktype = SOCK_DGRAM;
+    addrCriteris.ai_protocol = IPPROTO_UDP;
+
+    struct addrinfo *servAddr;
+    int rtnVal = getaddrinfo(server, service, &addrCriteris, &servAddr);
+    if (rtnVal != 0)
+        DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
+
+    int sock = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
+    if (sock < 0)
+        DieWithSystemMessage("socket() failed");
+
+    struct sigaction handler;
+    handler.sa_handler = CatchAlarm;
+    if (sigfillset(&handler.sa_mask) < 0)
+        DieWithSystemMessage("sigfillset() failed");
+    handler.sa_flags = 0;
+
+    if (sigaction(SIGALRM, &handler, 0) < 0)
+        DieWithSystemMessage("sigaction() failed for SIGALRM");
+
+    ssize_t numBytes = sendto(sock, echoString, echoStringLen, 0, servAddr->ai_addr, servAddr->ai_addrlen);
+    if (numBytes < 0)
+        DieWithSystemMessage("sendto() failed");
+    else if (numBytes != echoStringLen)
+        DieWithUserMessage("sendto() error", "sent unexpected number of bytes");
+
+    struct sockaddr_storage fromAddr;
+    socklen_t fromAddrLen = sizeof(fromAddr);
+    alarm(TIMEOUT_SECS);
+    char buffer[1024 + 1];
+    while ((numBytes = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&fromAddr, fromAddrLen)) < 0) {
+        if (errno == EINTR) {
+            if (tries < MAXTRIES) {
+                numBytes = sendto(sock, echoString, echoStringLen, 0, (
+                struct sockaddr *)&servAddr->ai_addr, servAddr->ai_addrlen);
+                if (numBytes < 0)
+                    DieWithSystemMessage("sendto() failed");
+                else if (numBytes != echoStringLen)
+                    DieWithUserMessage("sendto() error", "sent expected number of bytes");
+
+            } else
+                DieWithUserMessage("No Response", "unable to communicate with server");
+        } else
+            DieWithSystemMessage("recvfrom() failed");
+    }
+
+    alarm(0);
+
+    buffer[echoStringLen] = '\0';
+    printf("Received: %s\n", buffer);
+
+    close(sock);
+    exit(0);
+}
+
+void CatchAlarm(int ignore) {
+    tries += 1;
+}
