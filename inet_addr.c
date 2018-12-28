eacc39cc36a456feb00702eb959bcb4c3445dd9e
tanwei <1113513062@qq.com>
2018-12-28 14:53:23 +0800

    inet_addr函数使用

--- inet_addr.c
+++ inet_addr.c
@@ -1,4 +1,25 @@
 //
 // Created by Administrator on 2018/12/28.
 //
+#include <stdio.h>
+#include <arpa/inet.h>
 
+int main(int argc, char *argv[]) {
+    char *addr1="1.2.3.4";
+    char *addr2="1.2.3.256";
+
+    unsigned long conv_addr=inet_addr(addr1);
+    if (conv_addr==INADDR_NONE) {
+        printf("Error occurred! \n");
+    } else {
+        printf("Network ordered integer addr: %#lx \n", conv_addr);
+    }
+
+    conv_addr=inet_addr(addr2);
+    if (conv_addr==INADDR_NONE) {
+        printf("Error occurred! \n");
+    } else {
+        printf("Network ordered integer addr: %#lx \n", conv_addr);
+    }
+    return 0;
+}
