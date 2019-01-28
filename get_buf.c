6eb047b6bca16009b576b10088a8d201a2d842e4
tanwei <1113513062@qq.com>
2019-01-28 15:21:13 +0800

    获取套接字缓存

--- get_buf.c
+++ get_buf.c
@@ -2,3 +2,40 @@
 // Created by Administrator on 2019/1/2.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[]) {
+    int sock;
+    socklen_t len;
+    int snd_buf, rcv_buf, stat;
+
+    len= sizeof(snd_buf);
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+
+    stat=getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+    if (stat) {
+        error_handling("getsockopt() error!");
+    }
+
+    len= sizeof(rcv_buf);
+    stat=getsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, &len);
+    if (stat) {
+        error_handling("getsockopt() error!");
+    }
+
+    printf("Input buffer size: %d\n", rcv_buf);
+    printf("Output buffer size: %d\n", snd_buf);
+    close(sock);
+    return 0;
+}
+
+void error_handling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
