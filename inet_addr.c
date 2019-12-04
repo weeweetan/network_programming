8b0cd2750856434299a5fc2121315df6134d7a33
tanwei <1113513062@qq.com>
2019-12-04 14:43:11 +0800

    添加测试用例

--- inet_addr.c
+++ inet_addr.c
@@ -5,14 +5,17 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    char *addr1="1.2.3.4";
-    char *addr2="1.2.3.256";
+    char *addr1="10.0.0.0";
+    char *addr2="10.255.255.255";
+    char *addr3="11.255.255.255";
 
     unsigned long conv_addr=inet_addr(addr1);
     if (conv_addr==INADDR_NONE) {
         printf("Error occurred! \n");
     } else {
         printf("Network ordered integer addr: %#lx \n", conv_addr);
+        printf("Network ordered integer addr: %lu \n", conv_addr);
+        printf("Host ordered integer addr: %u \n", ntohl(conv_addr));
     }
 
     conv_addr=inet_addr(addr2);
@@ -20,6 +23,18 @@ int main(int argc, char *argv[]) {
         printf("Error occurred! \n");
     } else {
         printf("Network ordered integer addr: %#lx \n", conv_addr);
+        printf("Network ordered integer addr: %lu \n", conv_addr);
+        printf("Host ordered integer addr: %u \n", ntohl(conv_addr));
     }
+
+    conv_addr=inet_addr(addr3);
+    if (conv_addr==INADDR_NONE) {
+        printf("Error occurred! \n");
+    } else {
+        printf("Network ordered integer addr: %#lx \n", conv_addr);
+        printf("Network ordered integer addr: %lu \n", conv_addr);
+        printf("Host ordered integer addr: %u \n", ntohl(conv_addr));
+    }
+
     return 0;
 }
