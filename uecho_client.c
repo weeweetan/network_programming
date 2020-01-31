240d0fcff2679cc44a833326407acb31e87c8637
tanwei <1113513062@qq.com>
2020-01-31 16:50:37 +0800

    优化输出信息

--- uecho_client.c
+++ uecho_client.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
         adr_sz = sizeof(from_adr);
         str_len=recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&from_adr, &adr_sz);
         message[str_len]=0;
-        printf("Message from server: %s", message);
+        printf("Message from server: %s\n", message);
     }
     close(sock);
     return 0;
