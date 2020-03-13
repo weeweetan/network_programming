81b7c68710223232f0b53a91ac3174a480001c90
tanwei <1113513062@qq.com>
2020-03-13 20:14:15 +0800

    更新测试用例

--- inet_ntoa.c
+++ inet_ntoa.c
@@ -6,20 +6,23 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    struct sockaddr_in addr1, addr2;
+    struct sockaddr_in addr1, addr2, addr3;
     char *str_ptr;
     char str_arr[20];
 
     addr1.sin_addr.s_addr=htonl(0x1020304);
     addr2.sin_addr.s_addr=htonl(0x1010101);
+    addr3.sin_addr.s_addr=htonl(16785407);
 
     str_ptr=inet_ntoa(addr1.sin_addr);
     strcpy(str_arr, str_ptr);
     printf("Dotted-Decimal notation1: %s\n", str_ptr);
 
-    inet_ntoa(addr2.sin_addr);
+    str_ptr=inet_ntoa(addr2.sin_addr);
     printf("Dotted-Decimal notation2: %s\n", str_ptr);
-    printf("Dotted-Decimal notation3: %s\n", str_arr);
+
+    str_ptr=inet_ntoa(addr3.sin_addr);
+    printf("Dotted-Decimal notation3: %s\n", str_ptr);
 
 }
 
